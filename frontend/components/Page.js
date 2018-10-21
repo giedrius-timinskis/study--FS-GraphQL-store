7bf93a85d615f7deaaadbb296065d03b17bc188a
Giedrius Timinskis <giedriustiminskis@gmail.com>
2018-10-21 18:07:03 +1100

    Header styling

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
 import Meta from './Meta';
 import Header from './Header';
+import { inject } from 'async';
 
 
 class Page extends Component {
   render() {
     return (
-      <StyledPage>
-        <Meta />
-        <Header />
-        <Inner>
-          {this.props.children}
-        </Inner>
-      </StyledPage>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>
+            {this.props.children}
+          </Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
@@ -22,7 +25,7 @@ class Page extends Component {
 export default Page;
 
 const theme = {
-  red: '#FF000',
+  red: '#FF0000',
   black: '#393939',
   grey: '#3A3A3A',
   lightgrey: '#E1E1E1',
@@ -31,13 +34,43 @@ const theme = {
   bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
 };
 
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
+
 const StyledPage = styled.div`
   background: white;
-  color: black;
+  color: ${props => props.theme.black};
 `;
 
 const Inner = styled.div`
-  max-width: 1000px;
+  max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
 `;
