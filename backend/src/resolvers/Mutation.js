952d7d326701653cb98afc731f1c16e68f87a7ae
Giedrius Timinskis <giedriustiminskis@gmail.com>
2018-11-11 17:39:58 +1100

    Add delete item functionality and single item pages

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -33,6 +33,20 @@ const Mutations = {
         id: args.id,
       }
     }, info);
+  },
+  async deleteItem(
+    parent,
+    args,
+    ctx,
+    info
+  ) {
+    const where = { id: args.id };
+    // 1. Find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+    // 2. Check if user owns item/has permissions
+    // TODO:
+    // 3. Delete it.
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -6,6 +6,7 @@ import formatMoney from '../lib/formatMoney';
 import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
+import DeleteItem from './DeleteItem';
 
 class Item extends Component {
   render() {
@@ -39,7 +40,7 @@ class Item extends Component {
             <a>Edit</a>
           </Link>
           <button>Add To Cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete this item</DeleteItem>
         </div>
       </ItemStyles>
     );
