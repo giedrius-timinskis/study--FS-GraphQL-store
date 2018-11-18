4a5878536d8fb88e1f2cdd3cd3964e7bd1fc3e69
Giedrius Timinskis <giedriustiminskis@gmail.com>
2018-11-18 18:18:17 +1100

    Add Signin form

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -74,6 +74,27 @@ const Mutations = {
 
     // Return the user to the browser
     return user;
+  },
+  async signin(parent, { email, password }, ctx, info) {
+    // 1. Check if user with the email exists
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No user found for email ${email}`);
+    }
+    // 2. Check if the password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error('Invalid password');
+    }
+    // 3. Generate the JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // 4. Set the cookie with the token
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+    // 5. Return the user
+    return user;
   }
 };
 
--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,29 +4,35 @@ import Link from 'next/link';
 import User from './User';
 
 const Nav = () => (
-  <NavStyled>
-    <User>
-      {({ data: { me } }) => {
-        if (me) return <p>{me.name}</p>;
-        return null;
-      }}
-    </User>
-    <Link href="/items">
-      <a>Items</a>
-    </Link>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
-    <Link href="/signup">
-      <a>Signup</a>
-    </Link>
-    <Link href="/orders">
-      <a>Orders</a>
-    </Link>
-    <Link href="/me">
-      <a>Account</a>
-    </Link>
-  </NavStyled>
+  <User>
+    {({ data: { me } }) => (
+      <NavStyled>
+        <Link href="/items">
+          <a>Items</a>
+        </Link>
+
+        {me && (
+          <>
+            <Link href="/sell">
+              <a>Sell</a>
+            </Link>
+            <Link href="/orders">
+              <a>Orders</a>
+            </Link>
+            <Link href="/me">
+              <a>Account</a>
+            </Link>
+          </>
+        )}
+
+        {!me && (
+          <Link href="/signup">
+            <a>Signup</a>
+          </Link>
+        )}
+      </NavStyled>
+    )}
+  </User>
 );
 
 export default Nav;
