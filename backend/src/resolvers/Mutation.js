b33d15e82b1fa1026b5880ea9363a76d6c884b21
Giedrius Timinskis <giedriustiminskis@gmail.com>
2018-11-25 18:08:01 +1100

    Add password reset flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,9 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { randomBytes } = require('crypto');
+const { promisify } = require('util');
+
+const { transport, createEmail } = require('../mail');
 
 // This is a RESOLVER for retrieving data via a GraphQL interface from Prisma
 // Look at ../generated/prisma.graphql for the API of methods available
@@ -99,6 +103,73 @@ const Mutations = {
   signout(parent, args, ctx, info) {
     ctx.response.clearCookie('token');
     return { message: 'Logged out succesfully. ' };
+  },
+  async requestReset(parent, args, ctx, info) {
+    // 1. Check if it's a real user
+    const user = await ctx.db.query.user({ where: { email: args.email } });
+
+    if (!user) {
+      throw new Error(`No user found for email ${args.email}`);
+    }
+
+    // 2. Set reset token and expiry on the user
+    const randomBytesPromisified = promisify(randomBytes);
+    const resetToken = (await randomBytesPromisified(20)).toString('hex');
+    const resetTokenExpiry = Date.now() + 3600000; // 1 Hour from now
+    const res = await ctx.db.mutation.updateUser({
+      where: { email: args.email },
+      data: { resetToken, resetTokenExpiry }
+    });
+    // 3. Email user the reset token
+    const mailRes = await transport.sendMail({
+      from: 'giedriustiminskis@gmail.com',
+      to: user.email,
+      subject: 'Your Password Reset Token',
+      html: createEmail(`
+        Your password reset token is here!\n\n
+        <a href="${process.env.FRONTEND_URL}/reset?resetToken=${resetToken}">Click here to reset.</a>
+      `)
+    });
+
+    // 4. Return the message
+    return { message: 'Password reset email sent.' };
+  },
+  async resetPassword(parent, args, ctx, info) {
+    // 1. Check if the passwords match
+    if (args.password !== args.confirmPassword) {
+      throw new Error('Passwords do not match.');
+    }
+    // 2. Check if reset token is legit
+    // 3. Check if reset token is expired
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetToken: args.resetToken,
+        resetTokenExpiry_gte: Date.now() - 3600000,
+      },
+    });
+    if (!user) {
+      throw new Error('This token is invalid or expired.');
+    }
+    // 4. Hash the new password
+    const password = await bcrypt.hash(args.password, 10);
+    // 5. Save the new password to the user and remove old resetToken fields
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { email: user.email },
+      data: {
+        password,
+        resetToken: null,
+        resetTokenExpiry: null,
+      },
+    });
+    // 6. Generate JWT
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+    // 7. Set the JWT cookie
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+    // 8. Return the new user
+    return updatedUser;
   }
 };
 
---
+++ frontend/pages/reset.js
@@ -0,0 +1,5 @@
+import Reset from '../components/Reset';
+
+const Sell = props => <Reset resetToken={props.query.resetToken} />;
+
+export default Sell;
